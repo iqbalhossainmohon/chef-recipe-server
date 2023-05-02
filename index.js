bddcfb5ee0dfae984cc5dc8bed5b5e0b680aad9d
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-02 19:44:10 +0600

    chef feck data file added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
+const chefData = require('./data/chef-data.json');
+
+app.use(cors());
 app.get('/', (req, res) => {
-    res.send('chef recipe server side is running ')
-})
+    res.send(chefData);
+});
 
 app.listen(port, ()=>{
     console.log(`Chef Recipe API is running on port: ${port}`);
